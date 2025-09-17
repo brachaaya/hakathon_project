ccfbcaaae49c67190b661a7c522793eebfd3f086
jbt <yabs1002@gmail.com>
2025-09-17 15:29:55 +0300

    commit game math

--- math_game_pygame.py
+++ math_game_pygame.py
@@ -1,40 +1,123 @@
+# import pygame
+# import button
+# import math_competition
+#
+# #create display window
+# SCREEN_HEIGHT = 500
+# SCREEN_WIDTH = 800
+#
+# screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+# pygame.display.set_caption('Button Demo')
+#
+# start_img = pygame.image.load('start_btn.png').convert_alpha()
+# exit_img = pygame.image.load('exit_btn.png').convert_alpha()
+#
+# #create button instances
+# start_button = button.Button(100, 200, start_img, 0.8)
+#
+#
+# #game loop
+# run = True
+# while run:
+#
+#     screen.fill((202, 228, 241))
+#
+#     if start_button.draw(screen):
+#         print('START')
+#         screen.blit(math_competition.one_min_competition() , (100,100))
+#
+#     #event handler
+#     for event in pygame.event.get():
+#         #quit game
+#         if event.type == pygame.QUIT:
+#             run = False
+#
+#     pygame.display.update()
+#
+# pygame.quit()
+
 import pygame
-import button
+import sys
 
+# pygame.init() will initialize all
+# imported module
+pygame.init()
 
+clock = pygame.time.Clock()
 
-#create display window
-SCREEN_HEIGHT = 500
-SCREEN_WIDTH = 800
+# it will display on screen
+screen = pygame.display.set_mode([600, 500])
 
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption('Button Demo')
+# basic font for user typed
+base_font = pygame.font.Font(None, 32)
+user_text = ''
 
-start_img = pygame.image.load('start_btn.png').convert_alpha()
-exit_img = pygame.image.load('exit_btn.png').convert_alpha()
+# create rectangle
+input_rect = pygame.Rect(200, 200, 140, 32)
 
-#create button instances
-start_button = button.Button(100, 200, start_img, 0.8)
-exit_button = button.Button(450, 200, exit_img, 0.8)
+# color_active stores color(lightskyblue3) which
+# gets active when input box is clicked by user
+color_active = pygame.Color('lightskyblue3')
 
+# color_passive store color(chartreuse4) which is
+# color of input box.
+color_passive = pygame.Color('chartreuse4')
+color = color_passive
 
-#game loop
-run = True
-while run:
+active = False
 
-	screen.fill((202, 228, 241))
+while True:
+    for event in pygame.event.get():
 
-	if start_button.draw(screen):
-		print('START')
-	if exit_button.draw(screen):
-		print('EXIT')
+        # if user types QUIT then the screen will close
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
 
-	#event handler
-	for event in pygame.event.get():
-		#quit game
-		if event.type == pygame.QUIT:
-			run = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if input_rect.collidepoint(event.pos):
+                active = True
+            else:
+                active = False
 
-	pygame.display.update()
+        if event.type == pygame.KEYDOWN:
 
-pygame.quit()
+            # Check for backspace
+            if event.key == pygame.K_BACKSPACE:
+
+                # get text input from 0 to -1 i.e. end.
+                user_text = user_text[:-1]
+
+            # Unicode standard is used for string
+            # formation
+            else:
+                user_text += event.unicode
+
+    # it will set background color of screen
+    screen.fill((255, 255, 255))
+
+    if active:
+        color = color_active
+    else:
+        color = color_passive
+
+    # draw rectangle and argument passed which should
+    # be on screen
+    pygame.draw.rect(screen, color, input_rect)
+
+    text_surface = base_font.render(user_text, True, (255, 255, 255))
+
+    # render at position stated in arguments
+    screen.blit(text_surface, (input_rect.x + 5, input_rect.y + 5))
+
+    # set width of textfield so that text cannot get
+    # outside of user's text input
+    input_rect.w = max(100, text_surface.get_width() + 10)
+
+    # display.flip() will update only a portion of the
+    # screen to updated, not full area
+    pygame.display.flip()
+
+    # clock.tick(60) means that for every second at most
+    # 60 frames should be passed.
+    clock.tick(60)
