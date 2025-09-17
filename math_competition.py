9ac2ca2ee71cdafe800c5a14717e55756a072f4d
jbt <yabs1002@gmail.com>
2025-09-17 13:54:01 +0300

    first commit

---
+++ math_competition.py
@@ -0,0 +1,19 @@
+import time
+
+def one_min_competition(calc):
+    start_time = time.time()
+    current = 0
+    count_true = 0
+    count_false = 0
+    while time.time() - start_time < 60 :
+        print(f"Solve: {calc[current][0]} : ")
+        answer = int(input())
+        if answer == calc[current][1] :
+            count_true += 1
+            print("CORRECT!")
+        else :
+            count_false += 1
+        current += 1
+
+
+    print(f"In one minute you solved : {count_false+count_true} \n You solved {count_true} correctly.")
